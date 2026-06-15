5d63fafb285a1a8e346f9e897aa7488871bf7202
sandhyagupta90111-oss <sandhyagupta90111@gmail.com>
2026-06-15 19:05:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('Happy Birthday Rishabh');
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "💛";
+
+    heart.style.position = "fixed";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.bottom = "-20px";
+
+    heart.style.fontSize =
+        (20 + Math.random() * 30) + "px";
+
+    heart.style.pointerEvents = "none";
+
+    heart.style.zIndex = "999";
+
+    document.body.appendChild(heart);
+
+    let pos = 0;
+
+    const animation = setInterval(() => {
+
+        pos++;
+
+        heart.style.bottom = pos + "px";
+
+        heart.style.opacity = 1 - pos / 900;
+
+        if (pos > 900) {
+
+            clearInterval(animation);
+
+            heart.remove();
+
+        }
+
+    }, 15);
+}
+
+setInterval(createHeart, 600);
